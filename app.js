5a7802fde006fc9e461358e2a7eedf9672ebe85b
Naman khandelwal <namankoolwal1411@gmail.com>
2024-07-28 15:12:58 +0530

    add rate limit

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const passport = require('passport');
 const User = require('./models/user.model');
 const flash = require('connect-flash');
 require('dotenv').config();
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
 
 var usersRouter = require('./routes/user.routes');
 const postsRouter = require('./routes/posts.routes');
